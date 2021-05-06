8ab730716c43eefd409b19b7cefdde83692b75fd
Paul Le Bras <paul.le-bras@epitech.eu>
2021-05-06 15:30:52 +0200

    add handleStart and handleData functions

--- Hedhie/ia-rumble.py
+++ Hedhie/ia-rumble.py
@@ -1,7 +1,28 @@
 import socketio
-
 sio = socketio.Client()
 
+
+### AI FUNCTIONS ###
+
+def handleData(data):
+    width = data['board']['width']
+    height = data['board']['height']
+    board = []
+    pawns = data['pawns']
+    senderId = data['senderId']
+
+    boardData = data['board']['data']
+    for index in range(len(boardData) - 1, 0, -1):
+        if (index in [19, 14, 9, 4]):
+            subArray = []
+        subArray.append(boardData[index])
+        if (index in [19, 14, 9, 4]):
+            board.append(subArray)
+
+    return width, height, board, pawns, senderId
+
+
+### SIO FUNCTIONS ###
 
 @sio.event
 def connect():
@@ -11,7 +32,7 @@ def connect():
 @sio.event
 def my_message(data):
     print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+    sio.emit('room', {'roomName': 'rumble'})
 
 
 @sio.event
@@ -19,6 +40,31 @@ def disconnect():
     print('disconnected from server')
 
 
+@sio.event
+def handleStart(startData):
+    parsedData = handleData(startData)
+
+    width = parsedData[0]
+    height = parsedData[1]
+    board = parsedData[2]
+    pawns = parsedData[3]
+    senderId = parsedData[4]
+
+    print('width =', width)
+    print('height =', height)
+    print('board =', board)
+    print('pawns =', pawns)
+    print('senderId =', senderId)
+
+
+### MAIN ###
+
 sio.connect("http://localhost:3000")
 my_message("")
-sio.disconnect()
+
+while True:
+    sio.on("start", handleStart)
+    input()
+
+# sio.wait()
+# sio.disconnect()
