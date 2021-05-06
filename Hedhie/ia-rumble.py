bf01048ffecfcfe63f46796fd27a91a806864d25
Paul Le Bras <paul.le-bras@epitech.eu>
2021-05-06 19:07:27 +0200

    play with inputs

--- Hedhie/ia-rumble.py
+++ Hedhie/ia-rumble.py
@@ -1,25 +1,56 @@
 import socketio
+
 sio = socketio.Client()
+
+globalBoard = []
+globalPawns = []
 
 
 ### AI FUNCTIONS ###
 
 def handleData(data):
+    
+
     width = data['board']['width']
     height = data['board']['height']
     board = []
     pawns = data['pawns']
     senderId = data['senderId']
 
+    print("data =", data)
+
     boardData = data['board']['data']
-    for index in range(len(boardData) - 1, 0, -1):
-        if (index in [19, 14, 9, 4]):
+    if len(boardData) != 20:
+        print('entering if')
+        for index in range(0, 20 - len(boardData)):
+            boardData.append(None)
+
+    print('boardData =', boardData)
+    for index in range(0, len(boardData)):
+        if (index in [0, 5, 15, 20]):
             subArray = []
+        # print("boardData[index] =", boardData[index])
+        # print('index =', index)
         subArray.append(boardData[index])
-        if (index in [19, 14, 9, 4]):
+        if (index in [4, 9, 14, 19]):
             board.append(subArray)
 
     return width, height, board, pawns, senderId
+
+
+def searchMoves():
+    """returns an array containing the position in the characterRepartition array of all rooms containing only one character"""
+    """ movePossibilities = []
+
+    for room, roomIndex in zip(characterRepartition, list(range(0, len(characterRepartition)))):
+        if len(room) == 1:
+            movePossibilities.append(roomIndex)
+
+    return movePossibilities """
+
+# def dumbMove():
+    
+
 
 
 ### SIO FUNCTIONS ###
@@ -41,30 +72,53 @@ def disconnect():
 
 
 @sio.event
+def handleTurn(data):
+    print('dataTurn =', data)
+    if data['senderId'] != data['current']:
+        return
+    print('A NOUS')
+    pawn = int(input('choose your pawn: '))
+    x = int(input('choose your x move: '))
+    y = int(input('choose your y move: '))
+
+    
+
+
+    sio.emit("move", { 'pawn': pawn, 'destination': {'x': x, 'y': y} })
+
+@sio.event
+def handleUpdate(data):
+    print('dataUpdate =', data)
+
+
+@sio.event
 def handleStart(startData):
+    global globalBoard
+    global globalPawns
     parsedData = handleData(startData)
 
     width = parsedData[0]
     height = parsedData[1]
-    board = parsedData[2]
-    pawns = parsedData[3]
+    globalBoard = parsedData[2]
+    globalPawns = parsedData[3]
+    print('globalBoard =', globalBoard)
     senderId = parsedData[4]
 
-    print('width =', width)
-    print('height =', height)
-    print('board =', board)
-    print('pawns =', pawns)
-    print('senderId =', senderId)
+    sio.on("update", handleUpdate)
+    sio.on("change_turn", handleTurn)
 
+    
 
 ### MAIN ###
 
 sio.connect("http://localhost:3000")
 my_message("")
 
-while True:
+stop = None
+while(stop != 'stop'):
     sio.on("start", handleStart)
-    input()
+    # stop = input('type stop to stop: ')
+
 
 # sio.wait()
-# sio.disconnect()
+sio.disconnect()
